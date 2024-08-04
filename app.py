26cfc14102bfe08f3ecfa8cdfeb5c87e9c1fb4a5
Harsh Pandhe <pandhemihir@gmail.com>
2024-08-04 05:40:54 +0530

    Update 002

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+import pyttsx3
 from PyDictionary import PyDictionary
+
+
+class Speaking:
+    def __init__(self):
+        self.engine = pyttsx3.init("sapi5")
+        self.voices = self.engine.getProperty("voices")
+        self.engine.setProperty("voice", self.voices[0].id)
+
+    def speak(self, audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
 
 
 class DictionaryApp:
     def __init__(self):
         self.dictionary = PyDictionary()
+        self.speaker = Speaking()
 
     def get_word_meaning(self, word):
         meaning = self.dictionary.meaning(word)
@@ -11,8 +24,10 @@ class DictionaryApp:
             for part_of_speech, definitions in meaning.items():
                 for definition in definitions:
                     print(f"{part_of_speech}: {definition}")
+                    self.speaker.speak(f"The {part_of_speech} meaning is: {definition}")
         else:
             print("No meaning found.")
+            self.speaker.speak("No meaning found.")
 
     def run(self):
         word = input("Enter a word: ").strip()
